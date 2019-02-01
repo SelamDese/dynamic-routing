0f66469012ab9fded226af3103a459d771aac82e
selam <selam0315@gmail.com>
2019-02-01 11:02:55 -0600

    file setup

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,6 @@
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import AnimalList from "./animal/AnimalList";
+import LocationList from "./location/LocationList";
+import EmployeeList from "./employee/EMployeeList";
+import OwnerList from "./owner/OwnerList";
